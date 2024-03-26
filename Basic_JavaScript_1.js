5697204e744c27bc3cbaa0c5eab86d24cf05279a
7k7 <45836591+7k7@users.noreply.github.com>
2024-03-26 08:03:45 -0700

    Initial commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World! This is an alert box using JS!");
+
+function My_First_Function() {
+    var String = "Kiss me, I am Irish";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
